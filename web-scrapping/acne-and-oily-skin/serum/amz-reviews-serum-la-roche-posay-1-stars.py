e46b0f7871418685bb6d5597a18612f969ab4dc5
Nabila Husna <91649919+rnabilahusna@users.noreply.github.com>
2024-04-27 12:31:46 +0800

    Add web scrapping code for acne and oily skin
    First phase of web scrapping

---
+++ web-scrapping/acne-and-oily-skin/serum/amz-reviews-serum-la-roche-posay-1-stars.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
+
+reviewlist = []
+
+def get_soup(url):
+    r = requests.get('http://localhost:8050/render.html', params={'url': url, 'wait': 2})
+    soup = BeautifulSoup(r.text, 'html.parser')
+    return soup
+
+
+def get_reviews(soup):
+    reviews = soup.find_all('div', {'data-hook': 'review'})
+    try:
+        for item in reviews:
+            try:
+                # Convert date str to dd/mm/yyy format
+                datetime_str = item.select_one('[data-hook="review-date"]').text.strip().split(' on ')[-1]
+                date = datetime.strptime(datetime_str, '%B %d, %Y').strftime("%d/%m/%Y")
+            except Exception as e:
+                date = 'N/A'
+
+            # Extract the helpful vote count
+            try:
+                helpful_count = int(item.find('span', {'data-hook': 'helpful-vote-statement'}).text.strip().split()[0])
+            except (AttributeError, ValueError, IndexError):
+                helpful_count = 0
+
+            review = {
+            'product': soup.title.text.replace('Amazon.co.uk:Customer reviews:', '').strip(),
+            'date': date,
+            'title': item.find('a', {'data-hook': 'review-title'}).text.strip(),
+            'rating':  float(item.find('i', {'data-hook': 'review-star-rating'}).text.replace('out of 5 stars', '').strip()),
+            'body': item.find('span', {'data-hook': 'review-body'}).text.strip(),
+            'helpful_count': helpful_count, 
+            }
+            reviewlist.append(review)
+    except:
+        pass
+
+for x in range(1,11):
+    soup = get_soup(f'https://www.amazon.com/Effaclar-Salicylic-Treatment-Minimize-Blemishes/product-reviews/B005IXG9Y0/ref=cm_cr_getr_d_paging_btm_next_{x}?ie=UTF8&reviewerType=all_reviews&filterByStar=one_star&pageNumber={x}')
+    print(f'Getting page: {x}')
+    get_reviews(soup)
+    print(len(reviewlist))
+    if not soup.find('li', {'class': 'a-disabled a-last'}):
+        pass
+    else:
+        break
+
+df = pd.DataFrame(reviewlist)
+df.to_csv('serum-oily-la-roche-posay-1-stars.csv', index=False)
+print('Finish.')
